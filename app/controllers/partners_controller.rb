bec53b31a51e673a3d1e6e8801ddc4ee126f7721
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2013-12-07 16:47:27 -0600

    Add completed CRUD actions to partners_controller.

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -11,20 +11,28 @@ class PartnersController < ApplicationController
 
 	def create
 		@partner = Partner.new(partner_params)
+		@partner.save
+		redirect_to partner_path
 	end
 
 	def edit
+		@partner = Partner.find(params[:id])
 	end
 
 	def update
+		@partner = Partner.find(params[:id])
+		@partner.update(partner_params)
+		redirect_to partner_path
 	end
 
-	def delete
+	def destroy
+		@partner = Partner.find(params[:id])
+		@partner.destroy
+		redirect_to partner_path
 	end
 
 private
 	def partner_params
-		require(:partner).permit(:first_name, :last_name, 
-			:email_address, :phone_number)
+		require(:partner).permit(:first_name, :last_name, :email_address, :phone_number)
 	end
 end
