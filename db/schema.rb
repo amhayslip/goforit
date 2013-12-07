803f23cf21ea4397fe991de78c3ac59a03b70f54
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2013-12-07 16:03:17 -0600

    Add profil_pic to User model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131207193543) do
+ActiveRecord::Schema.define(version: 20131207220046) do
 
   create_table "goals", force: true do |t|
     t.datetime "created_at"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20131207193543) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "profile_pic"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
