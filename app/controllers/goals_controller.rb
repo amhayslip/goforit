da85d37a92fa7e385e4d2e05834f7bccd903e7e6
amhayslip <amhayslip@gmail.com>
2013-12-08 14:46:30 -0600

    Add Partner Front-End

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -11,12 +11,20 @@ class GoalsController < ApplicationController
 		@goal = Goal.new
 	end
 	
+	# def create
+	# 	@goal = Goal.new(goal_params)
+	# 	@goal.save
+	# 	redirect_to root_path
+	# end
+	
 	def create
 		@goal = Goal.new(goal_params)
 		@goal.save
-		redirect_to root_path
+		redirect_to user_path(goal_params[:user_id])
 	end
-	
+
+
+
 	def edit
 		@goal = Goal.find(params[:id])
 	end
@@ -24,13 +32,14 @@ class GoalsController < ApplicationController
 	def update
 		@goal = Goal.find(params[:id])
 		@goal.update(goal_params)
-		redirect_to root_path
+		redirect_to user_path(goal_params[:user_id])
 	end
 	
 	def destroy
 		@goal = Goal.find(params[:id])
+		user = User.find(@goal.user_id)
 		@goal.destroy
-		redirect_to root_path
+		redirect_to user_path(user.id)
 	end
 	
 	private
